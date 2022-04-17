9f26741b2a2eb413c227c46bcd57ab44633d05d3
deced <you@example.com>
2022-04-17 08:44:29 +0300

    update

--- register.php
+++ register.php
@@ -1,9 +1,23 @@
 <?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require_once __DIR__ . '/Mailer/PHPMailer.php';
+require_once __DIR__ . '/Mailer/SMTP.php';
+require_once  __DIR__. '/Mailer/Exception.php';
+
 $data = $_POST;
+$email_regex = '/\w+@\w+\.\w+/';
 if (isset($_POST['submit'])) {
     $errors = array();
     if (empty($data['name'])) {
         $errors[] = 'Ошибка! Вы не заполнили Имя';
+    } else if (empty($data['email'])) {
+        $errors[] = 'Ошибка! Вы не заполнили Email';
+    } else if (!preg_match_all($email_regex, $data['email'])) {
+        $errors[] = 'Ошибка! Email должен иметь формат "ivanov@.com"';
     } else if (empty($data['password1'])) {
         $errors[] = 'Ошибка! Вы не заполнили пароль';
     } else if (empty($data['password2'])) {
@@ -23,6 +37,28 @@ if (isset($_POST['submit'])) {
             $sql = "INSERT INTO Users (Name, Password) VALUES ('$name', '$password')";
             $result = $db->query($sql);
             setcookie("LoginCookie", $data["name"]);
+
+            $mail = new PHPMailer(true);
+            $mail->isSMTP();
+            $mail->Host = 'smtp.yandex.ru';
+            $mail->SMTPAuth = true;
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;;
+            $mail->Port = 465;
+            $mail->Username = 'lab7.vt';
+            $mail->Password = 'Hk341KHkb41hl';
+            $mail_to = $data['email'];
+            $mail->setFrom('lab7.vt@yandex.ru', 'ljjj ihkh');
+            $mail->addAddress($mail_to, 'Receiver Name');
+            $mail->addReplyTo('lab7.vt@yandex.ru', 'ljjj ihkh');
+            $mail->IsHTML(true);
+
+            $mail->Subject = "Регистрация";
+            $mail->Body = '<h1>Добро пожаловать на сайт автошколы http://abv-azbuka.com/</h1> Регистрация прошла успешно';
+            $mail->AltBody = 'Plain text message body for non-HTML email client. Gmail SMTP email body.';
+
+            $mail->send();
+
+
             echo '<div class="check-label background-color-green">Регистрация прошла успешно</div>';
         }
         else
@@ -31,6 +67,7 @@ if (isset($_POST['submit'])) {
         echo '<div class="check-label background-color-red">' . array_shift($errors) . '</div>';
     }
 }
+
 
 $main_template = file_get_contents("templates/main.html");
 $register_form = file_get_contents("templates/register/register.html");
